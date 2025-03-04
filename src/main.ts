6d05feeec7291f01aef55606f9960790897bef74
runarissu <runarissu@gmail.com>
2025-03-04 17:37:29 +0700

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import Aurelia from "aurelia";
+import { MyApp } from "./my-app";
+import "@coreui/coreui-pro/dist/css/coreui.min.css";
+import "@coreui/coreui-pro/dist/js/coreui.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "../src/assets/style/global.css";
+
+Aurelia.app(MyApp).start();
