ecb564fd1f32c923f186133f91a83c33c4cc0d3d
JirakitFS <jirakit_t@fissionsoft.co.th>
2025-03-05 02:54:29 +0700

    update

--- src/my-app.ts
+++ src/my-app.ts
@@ -2,11 +2,7 @@ export class MyApp {
 	username: string = "exampleUser";
 	email: string = "example@example.com";
 	description: string = "Hello, I'm an Aurelia application. ";
-	technologies: string[{ id; name }] = [
-		{ id: "0", name: "Angular" },
-		{ id: "1", name: "Bootstrap" },
-		{ id: "2", name: "React.js" },
-		{ id: "3", name: "Vue.js" },
-	];
-	selectedTechnologies: string[] = ["1", "3"];
+	isDefaultChecked: boolean = true;
+	selectedRadio: number = 1;
+	isSwitchChecked: boolean = true;
 }
