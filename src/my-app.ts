40077ee9e057ec97db5ef5980792d54461437ec2
JirakitFS <jirakit_t@fissionsoft.co.th>
2025-03-04 18:35:42 +0700

    initial commit

--- src/my-app.ts
+++ src/my-app.ts
@@ -1,3 +1,12 @@
 export class MyApp {
-  public message = 'Hello World!';
+	username: string = "exampleUser";
+	email: string = "example@example.com";
+	description: string = "Hello, I'm an Aurelia application. ";
+	technologies: string[{ id; name }] = [
+		{ id: "0", name: "Angular" },
+		{ id: "1", name: "Bootstrap" },
+		{ id: "2", name: "React.js" },
+		{ id: "3", name: "Vue.js" },
+	];
+	selectedTechnologies: string[] = ["1", "3"];
 }
